95f6697e82cef96858d97c44ab537ad5599905b6
Kat Dale <dalek22@ecu.edu>
2023-07-20 12:12:36 -0400

    New method for getting sat data

--- Code/chorlophyllA_satellite_data.R
+++ Code/chorlophyllA_satellite_data.R
@@ -6,24 +6,30 @@ library(readxl)
 library(ncdf4)
 library(httr)
 library(dplyr)
+library(rerddapXtracto)
 
 setwd(dir = "C://Users/fire2/Downloads/ECU Internship '23/")
 
 # Make sure this URL is the date/time/location range that you want, and that it's the ".nc" version
 junk <-
-  GET("https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdMH1chlamday_R2022SQ.nc?chlor_a%5B(2022-12-16T00:00:00Z):1:(2022-12-16T00:00:00Z)%5D%5B(89.97916):1:(-89.97917)%5D%5B(-179.9792):1:(179.9792)%5D",
+  GET("https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdMH1chlamday_R2022SQ.nc?chlor_a%5B(2022-12-16):1:(2022-12-16T00:00:00Z)%5D%5B(89.97916):1:(-89.97917)%5D%5B(-179.9792):1:(179.9792)%5D",
       write_disk("chlorA.nc", overwrite = TRUE))
 
 # Open .nc file
-nc = nc_open("chlorA.nc")
+nc = nc_open('chlorA.nc')
 
 # Look at what the variable names are
 names(nc$var)
 
 # Extract chlorophyll data
+nc_variable1 = nc$var[[1]]
 chlor_a = ncvar_get(nc, nc$var[[1]])
+dim(chlor_a)
 
-
+# Extract dates, latitude, and longitude (check these to make sure they look right!)
+dates = as.POSIXlt(nc_variable1$dim[[3]]$vals, origin = "1970-01-01", tz="GMT") # I got the time origin by looking at the metadata on ERDDAP ("time_origin")
+lon = nc_variable1$dim[[1]]$vals
+lat = nc_variable1$dim[[2]]$vals
 
 # ------------------
 # Redo
@@ -32,24 +38,35 @@ chlor_a = ncvar_get(nc, nc$var[[1]])
 prediction_grid <- load(file = "C://KDale/Projects/Phenology/Analysis/PredictionGrids/Anoplopoma fimbria_grid.rdata")
 chlor_a = "https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdMH1chlamday_R2022SQ.nc?chlor_a%5B(2022-12-16T00:00:00Z):1:(2022-12-16T00:00:00Z)%5D%5B(89.97916):1:(-89.97917)%5D%5B(-179.9792):1:(179.9792)%5D"
 
-dates <- prediction_grid_roms %>% sf::st_drop_geometry() %>% subset(year >= 2002 & month >= 8)
-dates <- paste(dates$year, dates$month, 1, sep = "-")
-dates <- format(as.Date(dates), "%Y-%m-%d")
-lat = prediction_grid_roms$latitude
-lon = prediction_grid_roms$longitude
+prediction_grid_subset <- prediction_grid_roms %>% sf::st_drop_geometry()
+prediction_grid_subset$dates = paste0(prediction_grid_subset$year, "-", prediction_grid_subset$month, "-", 1, "T00:00:00Z")
+prediction_grid_subset$dates = as.POSIXct(prediction_grid_subset$dates)
+dates = prediction_grid_subset$dates
+lat = prediction_grid_subset$latitude
+lon = prediction_grid_subset$longitude
 
-tot = rep(NA, 4)
-pb <- txtProgressBar(min = 0, max = nrow(sablefish), char = "=", style = 3)
+# Redo #2
+chlor_a <- griddap(
+  datasetx = 'erdMH1chlamday_R2022SQ',
+  url = "https://coastwatch.pfeg.noaa.gov/erddap/",
+  fields=c('time', 'latitude',  'longitude', 'chlor_a'),
+  time = c("2002-07-16T00:00:00Z", "2022-12-16T00:00:00Z"), # This time format needs to be exactly the same as the dataset's
+  latitude = c(35,60),
+  longitude = c(-134,-117),
+  fmt = "nc",
+  store = disk()
+)
 
-for (i in 1:nrow(sablefish)) {
+#Create dataframe
+chlor.df <-data.frame(longitude=as.numeric(chlor_a$longitude),
+                     latitude=as.numeric(chlor_a$latitude),
+                     time=strptime(chlor_a$time, "%Y-%m-%dT%H:%M:%S"),
+                     chlor_a=as.numeric(chlor_a$chlor_a))
 
-  #this is where the URL is built:
-  url = paste(chlor_a, "[(", dates[i], "):1:(", dates[i], ")][(", lat[i], "):1:(", lat[i], ")][(", lon[i], "):1:(", lon[i], ")]", sep = "")
+dataInfo <- rerddap::info('erdMH1chlamday_R2022SQ', url = "https://coastwatch.pfeg.noaa.gov/erddap/")
+dataInfo
 
-  new = read.csv(url, skip = 2, header = FALSE)
-  tot = bind_rows(tot, new)
-
-  setTxtProgressBar(pb, i)
-
-}
-close(pb) # Close progress bar
+rxtracto(dataInfo, parameter = "chlor_a",
+         tcoord = dates,
+         xcoord = lon,
+         ycoord = lat)
