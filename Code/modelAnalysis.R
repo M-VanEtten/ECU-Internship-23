10fe293d8621f24cd186a7c91a9fe33f64c0d73f
Kat Dale <dalek22@ecu.edu>
2023-07-21 15:32:17 -0400

    New prediction grid (expanded geographic scope)

--- Code/modelAnalysis.R
+++ Code/modelAnalysis.R
@@ -1,4 +1,4 @@
-# Code written by Martina van Etten
+# Code written by Martina van Etten for GSK internship
 
 library(readxl)
 library(ggplot2)
@@ -7,14 +7,105 @@ library(dplyr)
 library(mapping)
 library(blockCV)
 library(sf)
+library(tidyr)
 #head(dataSablefishSDM) #-- to quick view data
 
-#prepare data-------------------------------------------------------------------
-setwd(dir = "C://Users/fire2/Downloads/ECU Internship '23/")
+#prepare data (function to combine Bering Sea and CCE -------------------------
+createSpeciesDataset <- function() {
 
-dataSablefishSDM <- readxl::read_xlsx("data/sablefish.xlsx")
+  species = "Anoplopoma fimbria"
 
-# create sf object
+  all_tows_roms <- read.csv("C://KDale/Projects/Phenology/Data/AllTows_200nm.csv")
+  beringSeaCTD <- read.csv("data/BASIS_CTD_48e9_1d95_4312.csv") %>%
+    group_by_at(c("station_id", "latitude", "longitude", "time")) %>%
+    summarize(surface_temp_oC = mean(primary_temperature), surface_sal_psu = mean(primary_salinity))
+
+  beringSea <- read.csv("data/BASIS_FishCatch_a759_d3cb_ab11.csv") %>%
+    subset(., gearGeneral == "CobbMWT") %>%
+    select(-c(latitude, longitude, time)) %>%
+    merge(., beringSeaCTD, all.y = T, by = c("station_id")) %>%
+    rename(larvae_count = total_catch_num) %>%
+    tidyr::separate(., col = time, into = c("date", "time"), sep = c("T")) %>%
+    mutate(latitude = coalesce(eq_latitude, latitude), longitude = coalesce(eq_longitude, longitude)) %>%
+    mutate(gearGeneral = "CobbMWT", scientific_name = "Anoplopoma fimbria", month = lubridate::month(date), year = lubridate::year(date),  day = lubridate::day(date), program = "BASIS") %>%
+    select(., c("program", "latitude", "longitude", "year", "month", "day", "time", "gearGeneral", "larvae_count", "surface_temp_oC", "surface_sal_psu"))
+
+  speciesData <- read.csv(file = "c://KDale/Projects/Phenology/Data/AllCruises_Combined_200nm.csv") %>%
+    subset(scientific_name == "Anoplopoma fimbria" & year >= 1995 & year <= 2019) %>%
+    merge(., all_tows_roms, all.y = TRUE) %>%
+    bind_rows(., beringSea) %>%
+    subset(gearGeneral != "MOCNESS") %>%
+    mutate(., presence = 1) %>%
+    mutate(., abundance_scaled = coalesce(
+      scale(larvae_10m2, center = F)[,1],
+      scale(larvae_m3, center = F)[,1],
+      scale(larvae_1000m3, center = F)[,1],
+      scale(larvae_count, center = F)[,1]
+    )) %>%
+    mutate(., abundance = coalesce(
+      larvae_10m2,
+      larvae_m3,
+      larvae_1000m3,
+      larvae_count
+    ))
+
+  # Get maximum extents of positive tows
+  maxLat = max(speciesData$latitude)
+  minLat = min(speciesData$latitude)
+  maxLon = max(speciesData$longitude)
+  minLon = min(speciesData$longitude)
+
+  # Replace all NA abundance values with zero (these are true zeroes)
+  speciesData <-  mutate(speciesData, presence = replace_na(presence, 0),
+                         abundance = replace_na(abundance, 0),
+                         abundance_scaled = replace_na(abundance_scaled, 0),
+                         scientific_name = species) %>%
+    mutate(sst = coalesce(surface_temp_oC, sst_roms), salinity = coalesce(surface_sal_psu, salinity_roms)) %>%
+    subset(sst != 0 & salinity != 0 & ssh_roms != 0) %>% # remove lower outliers
+    subset(sst <= 40 & salinity <= 40) %>%  # remove upper outliers
+    mutate(., sst_scaled = scale(sst)[,1], ssh_scaled = scale(ssh_roms)[,1], salinity_scaled = scale(salinity)[,1]) %>%  # Center and scale enviro data
+    mutate(., logN1 = log(abundance_scaled+1))
+
+  # Add Conus Albers coordinates
+  coords= cbind(speciesData$longitude, speciesData$latitude)
+  scale = 1000
+  albert_equal_area <- sf::sf_project(from = "EPSG:4326", to = 'EPSG:5070', pts = coords)/scale
+  speciesData$X = albert_equal_area[,1]
+  speciesData$Y = albert_equal_area[,2]
+
+  # Add time block
+  timeblocks <- read_xlsx("C://KDale/Projects/Phenology/Data/timeblocks.xlsx", sheet =  1)
+  speciesData <- merge(speciesData, timeblocks, by = "year")
+  speciesData$timeblock = factor(speciesData$timeblock, levels = c("1995-1999", "2000-2004", "2005-2009", "2010-2014", "2015-2019"))
+
+  return(speciesData)
+}
+#------------------------------------------------------------------------------
+# Run function above
+dataSablefishSDM <- createSpeciesDataset()
+
+# Subset data to higher latitudes
+dataSablefishSDM <- subset(dataSablefishSDM, latitude > 41)
+
+#create mesh -- use make_mesh()-------------------------------------------------
+mesh <- make_mesh(dataSablefishSDM, xy_cols = c("X", "Y"), n_knots = 200)
+
+#fit the model -- use sdmTMB()--------------------------------------------------
+fitSablefish <- sdmTMB(formula = logN1 ~ s(sst_scaled) + s(ssh_scaled) + as.factor(gearGeneral),
+                       spatial = "on",
+                       data = dataSablefishSDM,
+                       # control = sdmTMBcontrol(newton_loops = 1, nlminb_loops = 2),
+                       mesh = mesh, family = tweedie(link = "log"),
+                       silent = FALSE) #run center of gravity on this model
+
+sanity(fitSablefish)
+
+visreg::visreg(fitSablefish, xvar = "sst_scaled")
+visreg::visreg(fitSablefish, xvar = "ssh_scaled")
+
+#create plot--------------------------------------------------------------------
+
+#create sf object
 dataSablefishSF <- st_as_sf(x = dataSablefishSDM, coords = c("longitude", "latitude")) %>%
   st_set_crs(4326) %>% #geographic reference sys
   st_transform( "EPSG:5070") #set projected coords sys
@@ -22,28 +113,6 @@ dataSablefishSF <- st_as_sf(x = dataSablefishSDM, coords = c("longitude", "latit
 #download shapefile
 NSAmerica <- read_sf("data/North_South_America/North_South_America.shp") %>% st_union() %>%
   st_transform(., crs = "EPSG:5070")
-
-
-
-#create mesh -- use make_mesh()-------------------------------------------------
-mesh <- make_mesh(dataSablefishSDM, xy_cols = c("X", "Y"), n_knots = 100)
-
-#fit the model -- use sdmTMB()--------------------------------------------------
-fitSablefish <- sdmTMB(formula = logN1 ~ s(sst_scaled) + s(ssh_scaled)
-                       +as.factor(gearGeneral),
-                       data = dataSablefishSDM,
-                      # control = sdmTMBcontrol(newton_loops = 1, nlminb_loops = 2),
-                       mesh = mesh, family = tweedie(link = "log"),
-                       silent = FALSE) #run center of gravity on this model
-
-
-
-
-sanity(fitSablefish)
-
-#create plot--------------------------------------------------------------------
-
-visreg::visreg(fitSablefish, xvar = "sst_scaled", scale ="response")
 
 #plot map of NSAmerica + pos tows
 ggplot() +
@@ -98,43 +167,46 @@ model1$sum_loglik
 model.NoSalinity$sum_loglik
 
 #prediction---------------------------------------------------------------------
+source("Code/createPredictionGrid.R")
+# Create prediction grid -- this takes a long time! It will save the grid as part of the function
+createPredictionGrid(data = dataSablefishSDM, species = "Anoplopoma fimbria", path = "data/Anoplopoma_fimbria_grid.rdata")
 
 load(file = "data/Anoplopoma fimbria_grid.rdata")
+
+prediction_grid = subset(prediction_grid, latitude > 41 & gearGeneral != "MOCNESS")
 #try to plot(^^) sf object
 
-pSable <- predict(fitSablefish, newdata = prediction_grid_roms) %>%
+# Predict for the current time period on a full grid
+pSable <- predict(fitSablefish, newdata = prediction_grid) %>%
   group_by_at(c("region", "year", "latitude", "longitude", "X", "Y"
-                ,"timeblock", "sst_roms", "ssh_roms", "month")) %>%
+                ,"timeblock", "sst_roms", "ssh_roms", "month", "chlor_a")) %>%
   summarize(est = sum(est), est_rf = mean(est_rf),est_non_rf = mean(est_non_rf)) %>%
-  mutate(est_responsescale = fitSablefish$family$linkinv(est))
+  ungroup() %>% mutate(chlor_a_scaled = scale(chlor_a)[,1]) %>% # center and scale chlorophyll
+  mutate(est_chlor_diff = est - chlor_a_scaled) # calculate difference between predicted abundance and chlor_a
 
 save(fitSablefish, pSable, file = "Results/prediction_fit_sablefish.rdata")
 
 load(file = "Results/prediction_fit_sablefish.rdata")
 
+# ggplot(pSable, aes(X, Y, fill = exp(est))) +
+#   geom_raster() +
+#   scale_fill_viridis_c(trans = "sqrt")
 
-
-#head(pSable)
-
-ggplot(pSable, aes(X, Y, fill = exp(est))) +
-  geom_raster() +
-  scale_fill_viridis_c(trans = "sqrt")
-
-
-#sf object based on pridiction data
-
+#sf object based on prediction data
 sablefishPre <- st_as_sf(x = pSable, coords = c("longitude", "latitude")) %>%
   st_set_crs(4326) %>%
   st_transform("EPSG:5070")
 
+# Plot biomass
 ggplot() +
-  geom_sf(data = sablefishPre, aes(color = exp(est_responsescale))) +
+  geom_sf(data = sablefishPre, aes(color = exp(est))) +
   geom_sf(data = NSAmerica) +
   facet_wrap(~ timeblock, nrow=1) +
   #super enhances map to view GOA, CAN, NNAMERICA
-  xlim(min(dataSablefishSF$X)*1000-1000, max(dataSablefishSF$X)*1000+1000) +
-  ylim(min(dataSablefishSF$Y)*1000-1000, max(dataSablefishSF$Y)*1000+1000)
+  xlim(min(prediction_grid$X)*1000-1000, max(prediction_grid$X)*1000+1000) +
+  ylim(min(prediction_grid$Y)*1000-1000, max(prediction_grid$Y)*1000+1000)
 
+# CHALLENGE - Plot match-mismatch difference (hint: use code above and change "color = ...")
 
 
 
